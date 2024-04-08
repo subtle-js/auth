13715e76c4c29ec6ab82a51b829a62ad07e109f7
Socheat Sok <alex@socheat.net>
2024-04-08 18:25:44 +0700

    Add package metadata

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { GenericAuth } from "./generic"
+import { __SUBTLEJS_AUTH_NAME, __SUBTLEJS_AUTH_VERSION } from "./package"
 
 // Main export of the module
 export { auth }
@@ -9,10 +10,15 @@ export { auth }
  */
 class SubtleAuth {}
 
+class InternalAuth {
+    readonly __SUBTLEJS_AUTH_NAME = __SUBTLEJS_AUTH_NAME
+    readonly __SUBTLEJS_AUTH_VERSION = __SUBTLEJS_AUTH_VERSION
+}
+
 /**
  * The `Auth` class provides a set of methods to perform authentication operations.
  */
-class Auth {
+class Auth extends InternalAuth {
     /**
      * A read-only property returns a `SubtleAuth` which can then be used to perform subtle authentication operations.
      */
