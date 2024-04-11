88fe0cb96675c114887f034bc69d96d84ecbf151
Socheat Sok <alex@socheat.net>
2024-04-11 13:59:42 +0700

    Update src/InternalAuth.ts

--- src/InternalAuth.ts
+++ src/InternalAuth.ts
@@ -2,8 +2,8 @@ import { SubtleAuth } from "./SubtleAuth"
 import { __SUBTLEJS_AUTH_NAME, __SUBTLEJS_AUTH_VERSION } from "./package"
 
 export class InternalAuth {
-    private readonly __SUBTLEJS_AUTH_NAME: string = __SUBTLEJS_AUTH_NAME
-    private readonly __SUBTLEJS_AUTH_VERSION: string = __SUBTLEJS_AUTH_VERSION
+    private readonly __SUBTLEJS_PKG_NAME: string = __SUBTLEJS_AUTH_NAME
+    private readonly __SUBTLEJS_PKG_VERSION: string = __SUBTLEJS_AUTH_VERSION
 
     /**
      * A read-only property returns a `SubtleAuth` which can then be used to perform subtle authentication operations.
